cb6d6223633ecbfc6fc79a14a1dad85da9cee471
MariaSchinoni <maria.schinoni10@gmail.com>
2022-11-07 10:29:27 -0300

    complete project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicTacToeComponent
   ],
   imports: [
     BrowserModule,
